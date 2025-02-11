d28a7f3c21f7136a6566f5249aa3175418525b06
Alex Interrante-Grant <a.interrantegrant@gmail.com>
2025-02-10 22:26:35 -0500

    fix: back button (revert scrolling naviation behavior)

--- src/app/recipe-retrieve/recipe-retrieve.component.ts
+++ src/app/recipe-retrieve/recipe-retrieve.component.ts
@@ -1,6 +1,6 @@
 import { Component, input } from "@angular/core";
-import { CommonModule, Location } from "@angular/common";
-import { ActivatedRoute, Router } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { ActivatedRoute, RouterLink } from "@angular/router";
 
 import { Recipe } from "../types";
 import { TagListComponent } from "../tag-list/tag-list.component";
@@ -8,34 +8,16 @@ import { IngredientComponent } from "../ingredient/ingredient.component";
 
 @Component({
   selector: "app-recipe-retrieve",
-  imports: [TagListComponent, IngredientComponent, CommonModule],
+  imports: [TagListComponent, IngredientComponent, RouterLink, CommonModule],
   templateUrl: "./recipe-retrieve.component.html",
   styleUrl: "./recipe-retrieve.component.css",
 })
 export class RecipeRetrieveComponent {
-  constructor(
-    private route: ActivatedRoute,
-    private location: Location,
-    private router: Router,
-  ) {}
+  constructor(private route: ActivatedRoute) {}
 
   recipe: Recipe | null = null;
 
   ngOnInit() {
     this.recipe = this.route.snapshot.data["recipe"];
-  }
-
-  goBack() {
-    if (document.referrer) {
-      const location = new URL(document.location.href);
-      const referrer = new URL(document.referrer);
-
-      if (location.hostname === referrer.hostname) {
-        this.location.back();
-        return;
-      }
-    }
-
-    this.router.navigate(["/"]);
   }
 }
